670f415868a97ee0951e6ade499978f370e3ce01
Nyakinyua <wanyakinyua968@gmail.com>
2020-01-10 14:39:46 +0300

    create projects model and get all projects in the views.py

--- awards/models.py
+++ awards/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from pyuploadcare.dj.models import ImageField
 from django.contrib.auth.models import User
+import datetime as dt
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=50)
+    image = ImageField(blank=True,manual_crop="")
+    description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    # class Meta:
+    #     ordering = [pub_date]
+    
 class User_profile(models.Model):
     bio = models.CharField(max_length=40)
     profile_pic = ImageField(blank=True,manual_crop="")
@@ -11,8 +25,3 @@ class User_profile(models.Model):
     def __str__(self):
         return self.bio
 
-    # @classmethod
-    # def get_profile(cls):
-    #     profile =cls.objects.all()
-    #     return profile
-    
