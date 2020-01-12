71eef43d94e23c7a022f654568a621e61c1ad7f9
Nyakinyua <wanyakinyua968@gmail.com>
2020-01-12 17:50:47 +0300

    create review form to allow users to rate projects based on the fields defined

---
+++ awards/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import User_profile,Projects,Review
+from pyuploadcare.dj.forms import ImageField
+from django.contrib.auth.models import User
+
+
+class EditProfileForm(forms.ModelForm):
+    """
+    Form to edit user profile
+    """
+    class Meta:
+        model=User_profile
+        fields = ('bio','profile_pic','email','phone_number')
+
+class UpdateProfileForm(forms.ModelForm):
+    '''
+    Form to add user profile
+    '''
+    class Meta:
+        model = User_profile
+        fields = ('bio','profile_pic','email','phone_number')
+        
+class NewProjectForm(forms.ModelForm):
+    '''
+    Form that allows user to post new project
+    '''
+    class Meta:
+        model = Projects
+        fields = ('title','image','description','project_link') 
+        
+class ReviewForm(forms.ModelForm):
+    """
+    Form that allows user post a review for a project
+    """
+    class Meta:
+        model=Review
+        exclude = ['user','date']       
