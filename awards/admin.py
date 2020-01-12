f42ce6456b83ab41d2da876436dcb9d1704719d2
Nyakinyua <wanyakinyua968@gmail.com>
2020-01-12 17:49:49 +0300

    include models in the admin.py

--- awards/admin.py
+++ awards/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User_profile,Projects
+from .models import User_profile,Projects,Review,ProjectReview
 
 # Register your models here.
 admin.site.register(User_profile)
 admin.site.register(Projects)
+admin.site.register(ProjectReview)
