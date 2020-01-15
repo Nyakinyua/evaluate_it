6c1d84c756cbfdfa62c020ec70aa46ac9ac76688
Nyakinyua <wanyakinyua968@gmail.com>
2020-01-15 11:46:42 +0300

    deployment

--- evaluate/settings.py
+++ evaluate/settings.py
@@ -20,7 +20,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
 if config('MODE')=="dev":
-   DATABASES = {
+    
+    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DB_NAME'),
@@ -31,17 +32,17 @@ if config('MODE')=="dev":
        }
        
    }
-   else:
-       DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
+else:
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
