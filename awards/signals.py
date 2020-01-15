054710280126c7b251f73b9a103bbb392116ccfd
Nyakinyua <wanyakinyua968@gmail.com>
2020-01-15 10:49:41 +0300

    Add the profile model in the signals.py

--- awards/signals.py
+++ awards/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import User_profile
 
 
 @receiver(post_save, sender=User)
@@ -10,11 +10,11 @@ def create_profile(sender,instance,created,**kwargs):
   this is a function that creates a profile of a user after registration
   '''
   if created:
-    Profile.objects.create(user=instance)
+    User_profile.objects.create(user=instance)
     
 @receiver(post_save, sender=User)
 def save_profile(sender,instance, **kwargs):
   '''
   this is a fuunction that saves the profile after been made
   '''
-  instance.profile.save()
+  instance.user_profile.save()
